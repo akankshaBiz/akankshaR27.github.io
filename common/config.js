b9d3f3dcc83271a8ff2b11db404c6f8919f26aae
Akanksha Rawat <akanksha27aug@gmail.com>
2017-02-14 13:08:59 +0530

    Changing to https from http

--- common/config.js
+++ common/config.js
@@ -1,6 +1,6 @@
 var app = angular.module('liveScoreApp');
 app.constant('appConfig', {
-	footBallBase: 'http://api.football-data.org/v1/',
+	footBallBase: 'https://api.football-data.org/v1/',
 	footBallKey: 'e225fd62154641df886b29b946d8c36c'
 });
 app.directive('countdown', [
